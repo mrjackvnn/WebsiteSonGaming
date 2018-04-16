b1e029799644a19ca939617eedbbc2ae7c7550be
SonSiHo <mrjackvnn@gmail.com>
2018-04-16 18:07:44 +0700

    Admin
    Trang Admin

--- WebsiteSonGaming/WebsiteSonGaming/Models/MetaData/SanPhamMetaData.cs
+++ WebsiteSonGaming/WebsiteSonGaming/Models/MetaData/SanPhamMetaData.cs
@@ -16,24 +16,31 @@ namespace WebsiteSonGaming.Models
             public int masanpham { get; set; }
 
             [Display(Name = "Nhà Sản Xuất")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public Nullable<int> mansx { get; set; }
 
             [Display(Name = "Loại Sản Phẩm")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public Nullable<int> maloai { get; set; }
 
             [Display(Name = "Tên Sản Phẩm")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public string tensanpham { get; set; }
 
             [Display(Name = "Giá Bán")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public Nullable<double> giaban { get; set; }
 
             [Display(Name = "Ngày Cập Nhật")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public Nullable<System.DateTime> ngaycapnhat { get; set; }
 
             [Display(Name = "Hình Sản Phẩm")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public string hinhsanpham { get; set; }
 
             [Display(Name = "Thông Tin")]
+            [Required(ErrorMessage = "{0} Không được để trống")]
             public string thongtin { get; set; }
         }
     }
